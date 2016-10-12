44009db43ededb78aa7fbd894a14c634f6a0872b
JasonVann <jasonvanet@gmail.com>
2016-10-12 00:49:00 -0700

    Added 2nd version; debugging count

--- HW/HW2.py
+++ HW/HW2.py
@@ -41,22 +41,68 @@ def partition1(nums, lo, hi):
     #print 'c', nums, nums[p], p
     return i-1
     #return nums
+
+def partition2(nums, lo, hi):
+    # Pick the last element
+    #p = choose_pivot1(nums[lo:hi+1])
+    p = hi
+    #print nums, p
+    i = lo
+    #j = 0
+    for j in range(lo, hi+1, 1):
+        #print 'a', nums, j, p
+        if nums[j] < nums[p]:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+        #j += 1
+    #print 'b', nums, p, i
+    nums[p], nums[i] = nums[i], nums[p]
+    #print 'c', nums, nums[p], p
+    return i
+    #return nums
         
-def quick_sort(nums, lo, hi):
-    #if len(nums) <= 1:
-    #    return nums
+def quick_sort(nums, lo, hi, op):
+    #print 65, nums, lo, hi
+    if hi - lo <= 1:        
+        return 1
+    c1, c2, c3, c4 = 0, 0, 0, 0
     if lo < hi:
-        p = partition1(nums, lo, hi)
-        quick_sort(nums, lo, p - 1)
-        quick_sort(nums, p + 1, hi)
-    return nums
+        if op == 1:
+            p = partition1(nums, lo, hi)
+        elif op == 2:
+            p = partition2(nums, lo, hi)
+        print 70, p, lo, hi
+        c1 = quick_sort(nums, lo, p - 1, op)
+        c3 = (p-1) - lo  
+        c2 = quick_sort(nums, p + 1, hi, op)
+        print 78, nums, p, lo, hi
+        c4 = hi - (p+1) 
+        '''
+        c1 = max(c1, 0)
+        c2 = max(c2, 0)
+        c3 = max(c3, 0)
+        c4 = max(c4, 0)
+        
+        c1 = abs(c1)
+        c2 = abs(c2)
+        c3 = abs(c3)
+        c4 = abs(c4)
+        '''
+        print c1, c2, c3, c4, nums, lo, hi
+    return c1+c2+c3+c4
 
 test1 = [9,8,7,6,5,4,3,2,1, 11, 15,14,13,12,9,7,5,22]
-test2 = [9]
+test2 = [9,8]
 
-lines = [line.strip('\r\n') for line in open('QuickSort.txt')]
+test10 = [3,9,8,4,6,10,2,5,7,1]
+
+file_name = 'hw2_test10.txt' #'QuickSort.txt'
+lines = [line.strip('\r\n') for line in open(file_name)]
 lines = [int(line) for line in lines]
-test = lines
-#print test[:50]
-print quick_sort(test, 0, len(test) - 1)
+test =  lines[:]
+print test[:50]
+print 'v1', quick_sort(test, 0, len(test) - 1, 1)
+test = lines[:]
+#print 'v2', quick_sort(test, 0, len(test) - 1, 2)
+
                                   
