3c91781e3ce2c7ab5fefed8b166bfcae2c0fc16e
JasonVann <jasonvanet@gmail.com>
2016-10-16 17:30:19 -0700

    Fixed v1 & v2 of quick sort

--- HW/HW2.py
+++ HW/HW2.py
@@ -63,20 +63,29 @@ def partition2(nums, lo, hi):
         
 def quick_sort(nums, lo, hi, op):
     #print 65, nums, lo, hi
-    if hi - lo <= 1:        
-        return 1
-    c1, c2, c3, c4 = 0, 0, 0, 0
+    #if hi - lo <= 0:        
+    # return 0
+    c1, c2, c3, c4, c = 0, 0, 0, 0, 0
     if lo < hi:
         if op == 1:
             p = partition1(nums, lo, hi)
         elif op == 2:
-            p = partition2(nums, lo, hi)
-        print 70, p, lo, hi
+            nums[lo], nums[hi] = nums[hi], nums[lo]
+            p = partition1(nums, lo, hi)
+        elif op == 3:
+            p = choose_pivot3(nums)
+            nums[lo], nums[p] = nums[p], nums[lo]
+            p = partition1(nums, lo, hi)
+        #print 70, p, lo, hi
         c1 = quick_sort(nums, lo, p - 1, op)
-        c3 = (p-1) - lo  
+        c3 = (p-1) - lo
         c2 = quick_sort(nums, p + 1, hi, op)
-        print 78, nums, p, lo, hi
-        c4 = hi - (p+1) 
+        #print 78, nums, p, lo, hi
+        c4 = hi - (p+1)
+        c = hi - lo + 1
+        # each quick_sort call has a (lo, hi) comparison
+        c1 += 1
+        c2 += 1
         '''
         c1 = max(c1, 0)
         c2 = max(c2, 0)
@@ -88,21 +97,23 @@ def quick_sort(nums, lo, hi, op):
         c3 = abs(c3)
         c4 = abs(c4)
         '''
-        print c1, c2, c3, c4, nums, lo, hi
-    return c1+c2+c3+c4
+        #print c1, c2, c3, c4, nums, lo, hi, c
+    return c1 + c2 + c3 + c4
 
 test1 = [9,8,7,6,5,4,3,2,1, 11, 15,14,13,12,9,7,5,22]
 test2 = [9,8]
 
 test10 = [3,9,8,4,6,10,2,5,7,1]
 
-file_name = 'hw2_test10.txt' #'QuickSort.txt'
+file_name = 'hw2_test1000.txt' #'QuickSort.txt'
 lines = [line.strip('\r\n') for line in open(file_name)]
 lines = [int(line) for line in lines]
 test =  lines[:]
-print test[:50]
-print 'v1', quick_sort(test, 0, len(test) - 1, 1)
+#test = test2
+#print 'v1', quick_sort(test, 0, len(test) - 1, 1)
+#print test
 test = lines[:]
-#print 'v2', quick_sort(test, 0, len(test) - 1, 2)
+print 'v2', quick_sort(test, 0, len(test) - 1, 3)
+print test[:50]
 
                                   
